9ef41e4dff0f667739e0cbe9e96b60eeda6fac97
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-11-07 22:25:49 -0500

    Video 7

--- 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
+++ 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -20,9 +21,13 @@ export class RegistroComponent implements OnInit {
     this.usuario.email = 'chiquil092006@gmail.com';
   }
 
-  onSubmit() {
+  onSubmit( form: NgForm ) {
+
+    if ( form.invalid ) { return; }
+
     console.log('Formulario Enviado');
     console.log(this.usuario);
+    console.log(form);
   }
 
 }
