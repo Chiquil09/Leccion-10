dec26e9b275f716a28a177cefd02749da33d7673
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-11-08 02:35:57 -0500

    Video 18

--- 07-bonus-loginApp/src/app/guards/auth.guard.ts
+++ 07-bonus-loginApp/src/app/guards/auth.guard.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor( private auth: AuthService,
-               private router: Router) {}
+                private router: Router) {}
 
   canActivate(): boolean  {
 
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
       this.router.navigateByUrl('/login');
       return false;
     }
- 
+
   }
 
 }
